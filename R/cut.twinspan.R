1c734b0323ddf1dff052dcd78e1f2ecea8312575
Jari Oksanen <jari.oksanen@oulu.fi>
2019-08-12 23:36:07 +0300

    add cut.twinspan to extract classification at a given level of hierarchy
    This is a S3 method, although twinspan has no class yet and its
    classification vector is of wrong length. Twinspan will return
    only the result of last division (last level of hierarchy), but
    this can be used to recover classification at any lower level.

---
+++ R/cut.twinspan.R
@@ -0,0 +1,29 @@
+#' Return twinspan Classification at Given Level
+#'
+#' Function returns a vector of \code{twinspan} classes at given level
+#' of hierarcy for quadrats.
+#' 
+#' @param x \code{twinspan} result.
+#' @param level Level of hierarchy for classification. If missing, the
+#'     highest lvel used in the object will be returned.
+#' @param \dots Other parameters (ignored).
+#'
+#' @return A vector of class numbers for the given level of hierarchy
+#'     using Twinspan identifiers. A mother class can be found through
+#'     integer division by 2 for a given class identifier.
+#' @export
+`cut.twinspan` <-
+    function(x, level, ...)
+{
+    if (missing(level))
+        level <- 31L
+    clmax <- 2^(level+1) - 1
+    cl <- x$iclass
+    big <- cl > clmax
+    while(any(big)) {
+        ## mother class by integer division
+        cl[big] <- cl[big] %/% 2
+        big <- cl > clmax
+    }
+    cl
+}
