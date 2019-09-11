efbcd69aace71f109f1824715e3e53c5588d6cbb
Jari Oksanen <jari.oksanen@oulu.fi>
2019-09-11 14:54:15 +0300

    cut works alternatively for quadrat or species classification

--- R/cut.twinspan.R
+++ R/cut.twinspan.R
@@ -2,10 +2,12 @@
 #'
 #' Function returns a vector of \code{twinspan} classes at given level
 #' of hierarcy for quadrats.
-#' 
+#'
 #' @param x \code{twinspan} result.
 #' @param level Level of hierarchy for classification. If missing, the
 #'     highest lvel used in the object will be returned.
+#' @param what Return either a \code{"quadrat"} or \code{"species"}
+#'     classification vector.
 #' @param \dots Other parameters (ignored).
 #'
 #' @return A vector of class numbers for the given level of hierarchy
@@ -13,12 +15,13 @@
 #'     integer division by 2 for a given class identifier.
 #' @export
 `cut.twinspan` <-
-    function(x, level, ...)
+    function(x, level, what = c("quadrat", "species"), ...)
 {
+    what <- match.arg(what)
     if (missing(level))
         level <- 31L
     clmax <- 2^(level+1) - 1
-    cl <- x$iclass
+    cl <- x[[what]]$iclass
     big <- cl > clmax
     while(any(big)) {
         ## mother class by integer division
