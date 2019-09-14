290600d17e03d6c373f4b78a63ff355908cab223
Jari Oksanen <jari.oksanen@oulu.fi>
2019-09-14 18:33:37 +0300

    recursive function to report twinspan classification: body & POC

--- R/summary.twinspan.R
+++ R/summary.twinspan.R
@@ -2,41 +2,31 @@
 #'
 #' @export
 `summary.twinspan` <-
-    function(object, what = c("quadrat", "species"), ...)
+    function(object, what = c("quadrat","species"), ...)
 {
     what <- match.arg(what)
     obj <- object[[what]]
     clid <- cut(object, what=what)
-    len <- length(obj$eig) * 2 + 1
+    len <- length(obj$eig) * 4 + 1
     state <- character(len)
     state[which(obj$eig > 0)] <- "division"
     state[unique(clid)] <- "cluster"
-    z <- list()
-    for(k in rev(seq_along(state))) {
-        if(nchar(state[k]) == 0)
-            next
-        if(state[k] == "cluster") {
-            items <- obj$labels[clid==k]
-            N <- length(items)
-            zk <- list(type = "twinclus",
-                       items = items,
-                       N = N, k = k)
-        }
-        else { # a division
-            kids <- c(2*k, 2*k+1)
-            kids <- as.character(kids)
-            N <- z[[kids[1]]]$N + z[[kids[2]]]$N
-            eig <- obj$eig[k]
-            i <- obj$indicators[,k]
-            i <- i[i != 0]
-            inds <- paste0(c("-","+")[(i>0)+1], obj$indlabels[abs(i)])
-            positivelimit <- obj$positivelimit[k]
-            zk <- list(type = "twindiv", N = N, eig = eig, indicator = inds,
-                       positivelimit = positivelimit, k = k,
-                       kids=list(z[[kids[1]]], z[[kids[2]]]))
-            z[[kids[1]]] <- z[[kids[2]]] <- NULL
-        }
-        z[[as.character(k)]] <- zk
-    }
-    structure(z[[as.character(k)]], class="summary.twinspan")
+    o <- twinvisit(1, state)
+}
+
+
+`twinvisit` <-
+    function(k, state)
+{
+    if(state[k]=="")
+        return(NULL)
+    twinreport(k, state)
+    twinvisit(2*k, state)
+    twinvisit(2*k+1, state)
+}
+
+`twinreport` <-
+    function(k, state)
+{
+    cat(k, ") ", state[k], "\n", sep="")
 }
