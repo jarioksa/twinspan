ea4f681fc7415a8318bc4c2c9503078d22ffd8ab
Jari Oksanen <jari.oksanen@oulu.fi>
2019-09-20 17:33:14 +0300

    add argument to 'select' only some quadrats

--- R/twintable.R
+++ R/twintable.R
@@ -9,20 +9,25 @@
 #' @param maxspp Maximum number of most abundant species
 #'     displayed. The abundance is estimated with pseudospecies cut
 #'     levels. The default is to show all species.
+#' @param select Select a subset of quadrats.
 #'
 #' @importFrom vegan vegemite
 #'
 #' @export
 `twintable` <-
-    function(object, maxspp)
+    function(object, maxspp, select)
 {
     i <- object$quadrat$index
     j <- object$species$index
     mat <- twin2mat(object)
+    ## select first a subset
+    if (!missing(select)) {
+        i <- i[select[i]]
+    }
     ## take only maxspp most abundant species - but if there are ties,
     ## take all tied species, even if we go over maxspp
     if (!missing(maxspp) && ncol(mat) > maxspp) {
-        sptot <- colSums(mat)
+        sptot <- colSums(mat[i,])
         maxlim <- sort(sptot, decreasing = TRUE)[maxspp]
         j <- j[sptot[j] >= maxlim]
     }
