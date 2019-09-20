f7c9103ce5175a9ff904a0656ebf73ed0889b75e
Jari Oksanen <jari.oksanen@oulu.fi>
2019-09-20 12:42:32 +0300

    export & rudiments of documentation of the POC version of twintable

--- R/twintable.R
+++ R/twintable.R
@@ -1,10 +1,20 @@
 ### TWINSPAN table
 
+#' Community Table Ordered by Twinspan Classification
+#'
+#' Prints a community table of pseudospecies ordered by
+#' \code{\link{twinspan}} classification.
+#'
+#' @param object \code{\link{twinspan}} result object.
+#'
+#' @importFrom vegan vegemite
+#'
+#' @export
 `twintable` <-
     function(object)
 {
     i <- object$quadrat$index
     j <- object$species$index
     mat <- twin2mat(object)
-    vegan::vegemite(mat[i,j], zero="-")
+    vegemite(mat[i,j], zero="-")
 }
