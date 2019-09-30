f5d4eede5819e176de7be108e356d913f1c4c54e
Jari Oksanen <jari.oksanen@oulu.fi>
2019-09-30 15:29:30 +0300

    new function to identify misclassified quadrats & reporting on them

---
+++ R/misclassified.R
@@ -0,0 +1,96 @@
+### report "misclassified" observations
+#'
+#' Report Misclassified Quadrats
+#'
+#' \code{\link{twinspan}} bases its quadrat classification primarily
+#' on ordination axis. In some cases this is in conflict with the
+#' classification derived from indicator pseudospecies. This function
+#' identifies these cases.
+#'
+#' The function compares the final \code{\link{twinspan}}
+#' classification (from \code{\link{cut.twinspan}}) and the
+#' classification predicted from indicator pseudospecies (from
+#' \code{\link{predict.twinspan}}). If these two differ, the quadrat is
+#' \dQuote{misclassified}. With \code{verbose=FALSE}, the function
+#' returns only the index of misclassified quadrats. In default, it
+#' also returns the names, twinspan classes and predicted classes of
+#' misclassified quadrats, and the division level where the
+#' misclassification ocurred and classifications diverged.
+#'
+#' @return
+#'
+#' If \code{verbose=TRUE}, the function returns an object of class
+#' \code{"misclassified"} with following elements.
+#' \describe{
+#' \item{index}{Index of misclassified quadrats.}
+#' \item{labels}{Labels (names) of quadrats.}
+#' \item{class}{Final classification from \code{\link{twinspan}}.}
+#' \item{predicted}{Final classification from \code{\link{predict.twinspan}}.}
+#' \item{diverged}{The division level at which the misclassification occurred.}
+#' }
+#'
+#' @seealso \code{\link{cut.twinspan}}, \code{\link{predict.twinspan}}.
+#'
+#' @examples
+#'
+#' data(ahti)
+#' tw <- twinspan(ahti)
+#' misclassified(tw)
+#'
+#' @param x \code{\link{twinspan}} result object.
+#' @param verbose If \code{FALSE}, returns only the index of
+#'     misclassified quadrats.
+#'
+#' @export
+`misclassified` <-
+    function(x, verbose = TRUE)
+{
+    class <- cut(x)
+    pred <- predict(x)
+    # misclassified cases
+    ind <- which(class != pred)
+    if (!verbose)
+        return(ind)
+    ## take only misclassified cases
+    labs <- x$quadrat$labels[ind]
+    class <- class[ind]
+    pred <- pred[ind]
+    ## see the level of divergence
+    level <- x$levelmax
+    div <- rep(level, length(ind))
+    cl <- class
+    pr <- pred
+    for(lev in rev(seq_len(level-1))) {
+        lim <- 2^(lev+1) - 1
+        pr[pr > lim] <- pr[pr > lim] %/% 2L
+        cl[cl > lim] <- cl[cl > lim] %/% 2L
+        k <- pr != cl
+        if (any(k)) {
+            div[k] <- lev
+        } else {
+            break
+        }
+    }
+    out <- list(index = ind, labels = labs, class = class, predicted = pred,
+                diverged = div)
+    class(out) <- "misclassified"
+    out
+}
+
+#' @export
+`print.misclassified` <-
+    function(x, ...)
+{
+    nmiss <- length(x$index)
+    if (nmiss == 0)
+        cat("\nNo misclassified quadrats\n\n")
+    else {
+        cat("\n", nmiss, "misclassified quadrats and their level of divergence:\n\n")
+        out <- data.frame("ind"=x$index, "Quadrat" = x$labels,
+                          "Class" = x$class, "Predicted" = x$predicted,
+                          "Divergence" = x$diverged)
+        names(out)[1] <- ""
+        print(out, row.names = FALSE)
+    }
+    invisible(x)
+}
