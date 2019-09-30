147ee8ba6539ffc476f8ef7f8031b03ecd9a7fa6
Jari Oksanen <jari.oksanen@oulu.fi>
2019-09-30 16:51:31 +0300

    return the division of misclassification instead of its level

--- R/misclassified.R
+++ R/misclassified.R
@@ -10,12 +10,14 @@
 #' The function compares the final \code{\link{twinspan}}
 #' classification (from \code{\link{cut.twinspan}}) and the
 #' classification predicted from indicator pseudospecies (from
-#' \code{\link{predict.twinspan}}). If these two differ, the quadrat is
-#' \dQuote{misclassified}. With \code{verbose=FALSE}, the function
+#' \code{\link{predict.twinspan}}). If these two differ, the quadrat
+#' is \dQuote{misclassified}. With \code{verbose=FALSE}, the function
 #' returns only the index of misclassified quadrats. In default, it
 #' also returns the names, twinspan classes and predicted classes of
-#' misclassified quadrats, and the division level where the
-#' misclassification ocurred and classifications diverged.
+#' misclassified quadrats, and the division where the
+#' misclassification occurred and classifications diverged. The
+#' divisions and their numbers can be seen in
+#' \code{\link{summary.twinspan}} and \code{\link{plot.twinspan}}.
 #'
 #' @return
 #'
@@ -26,10 +28,12 @@
 #' \item{labels}{Labels (names) of quadrats.}
 #' \item{class}{Final classification from \code{\link{twinspan}}.}
 #' \item{predicted}{Final classification from \code{\link{predict.twinspan}}.}
-#' \item{diverged}{The division level at which the misclassification occurred.}
+#' \item{division}{The division where the misclassification occurred.}
 #' }
 #'
-#' @seealso \code{\link{cut.twinspan}}, \code{\link{predict.twinspan}}.
+#' @seealso \code{\link{cut.twinspan}},
+#'     \code{\link{predict.twinspan}}, \code{\link{summary.twinspan}},
+#'     \code{\link{plot.twinspan}}.
 #'
 #' @examples
 #'
@@ -57,22 +61,23 @@
     pred <- pred[ind]
     ## see the level of divergence
     level <- x$levelmax
-    div <- rep(level, length(ind))
+    div <- numeric(length(ind))
     cl <- class
     pr <- pred
-    for(lev in rev(seq_len(level-1))) {
+    for(lev in seq(level-1, 0)) {
         lim <- 2^(lev+1) - 1
         pr[pr > lim] <- pr[pr > lim] %/% 2L
         cl[cl > lim] <- cl[cl > lim] %/% 2L
-        k <- pr != cl
-        if (any(k)) {
-            div[k] <- lev
-        } else {
+        ## prediction and class match for the first time
+        k <- pr == cl & div == 0
+        if (any(k))
+            div[k] <- pr[k]
+        ## all done?
+        if (all(div > 0))
             break
-        }
     }
     out <- list(index = ind, labels = labs, class = class, predicted = pred,
-                diverged = div)
+                division = div)
     class(out) <- "misclassified"
     out
 }
@@ -85,10 +90,11 @@
     if (nmiss == 0)
         cat("\nNo misclassified quadrats\n\n")
     else {
-        cat("\n", nmiss, "misclassified quadrats and their level of divergence:\n\n")
+        cat("\n", nmiss,
+            "misclassified quadrats and their level of divergence:\n\n")
         out <- data.frame("ind"=x$index, "Quadrat" = x$labels,
                           "Class" = x$class, "Predicted" = x$predicted,
-                          "Divergence" = x$diverged)
+                          "Division" = x$division)
         names(out)[1] <- ""
         print(out, row.names = FALSE)
     }
