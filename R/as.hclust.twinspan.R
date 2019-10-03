4c2f28b662b0e8ae56a95cb7f940de75f82dcfb3
Jari Oksanen <jari.oksanen@oulu.fi>
2019-10-03 12:18:16 +0300

    plot.twinspan gained argument 'what' & expanded documentation

--- R/as.hclust.twinspan.R
+++ R/as.hclust.twinspan.R
@@ -79,9 +79,38 @@
 
 ### plot.twinspan as plot of hclust tree
 
-#' Plot Classificaton Tree of Quadrats
+#' Plot Classification Tree
+#'
+#' Function displays the classification tree.
+#'
+#' The internal nodes are labelled by the numbers of division. These
+#' are the same numbers as used in \code{\link{summary.twinspan}} and
+#' returned by \code{\link{cut.twinspan}} or
+#' \code{\link{predict.twinspan}} for the same classification
+#' level. For terminal groups the plot shows the number of the group
+#' and the number of items (quadrats or species) in the group. For
+#' division number \eqn{k}, its daughter divisions or groups are
+#' \eqn{2k}{2*k} and \eqn{2k+1}{2*k+1}. The tree is similar as a plot
+#' of \code{\link{as.hclust.twinspan}}, but adds numbers of internal
+#' nodes.
+#'
+#' @seealso \code{\link{summary.twinspan}} for similar textual
+#'     presentation also showing the items (quadrats, species) in
+#'     terminal groups. \CRANpkg{vegan} function
+#'     \code{\link[vegan]{scores.hclust}} can extract the coordinates
+#'     of internal (or terminal nodes), and
+#'     \code{\link[vegan]{ordilabel}} is used add the labels on
+#'     internal nodes.
+#'
+#' @examples
+#'
+#' data(ahti)
+#' tw <- twinspan(ahti)
+#' plot(tw, "species")
 #'
 #' @param x \code{\link{twinspan}} result object.
+#' @param what Plot \code{"quadrat"} or \code{"species"}
+#'     classification tree.
 #' @param main Main title of the plot.
 #' @param \dots Other parameters passed to \code{\link{plot}} and
 #'     \code{\link[vegan]{ordilabel}}.
@@ -91,9 +120,11 @@
 #'
 #' @export
 `plot.twinspan` <-
-    function(x, main = "Twinspan Dendrogram", ...)
+    function(x, what = c("quadrat", "species"), main = "Twinspan Dendrogram",
+             ...)
 {
-    x <- as.hclust(x)
+    what <- match.arg(what)
+    x <- as.hclust(x, what = what)
     plot(x, main = main, ...)
     ordilabel(x, "internal", labels = x$nodelabels, ...)
 }
