3abf4fbbeffc3e20cf5e144456203a95226d3538
Jari Oksanen <jari.oksanen@oulu.fi>
2019-10-06 18:34:38 +0300

    register compiled code, but then devtools fails and you need kluges
    Strict --as-cran tests requires registration of entry points in
    compiled code, but devtools does not known how to do this, and
    you get an error if you try to rebuild your documentation and
    NAMESPACE. Instead of simple devtools::document('.') you need to have
    Rscript -e 'pkgload::load_all(); devtools::document(".")' which will
    soil your src/ with object files and then you need git clean -f to
    get clean directories.

---
+++ src/init.c
@@ -0,0 +1,47 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> /* for NULL */
+#include <R_ext/Rdynload.h>
+
+/* extern definitions */
+
+extern void data2hill(void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(pseudo)(void *, void *,void *, void *, void *, void *,
+    void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(class)(void *, void *, void *, void *, void *, void *,
+			    void *, void *, void *, void *, void *, void *,
+			    void *, void *, void *, void *, void *, void *,
+			    void *, void *, void *, void *, void *, void *,
+			    void *, void *, void *, void *, void *, void *,
+			    void *, void *, void *, void *, void *, void *,
+			    void *, void *, void *, void *);
+extern void F77_NAME(makejdat)(void *, void *, void *, void *, void *, void *,
+			       void *, void *, void *, void *, void *, void *,
+			       void *);
+extern void F77_NAME(revspec)(void *, void *, void *, void *, void *, void *,
+			      void *, void *, void *);
+
+/* C */
+
+static const R_CMethodDef CCalls[] = {
+    {"data2hill", (DL_FUNC) &data2hill, 6},
+    {NULL, NULL, 0}
+};
+
+/* FORTRAN */
+
+static const R_FortranMethodDef FortranCalls[] = {
+    {"pseudo", (DL_FUNC) &F77_NAME(pseudo), 13},
+    {"class", (DL_FUNC) &F77_NAME(class), 40},
+    {"makejdat", (DL_FUNC) &F77_NAME(makejdat), 13},
+    {"revspec", (DL_FUNC) &F77_NAME(revspec), 9},
+    {NULL, NULL, 0}
+};
+
+/* registration */
+
+void R_init_twinspan(DllInfo *dll)
+{
+    R_registerRoutines(dll, CCalls, NULL, FortranCalls, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
