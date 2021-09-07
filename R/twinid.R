851d2ee341f6dd37922c37821dab86f78ba21dff
Jari Oksanen <jhoksane@gmail.com>
2021-09-07 13:45:35 +0300

    add twinid: label hclust objects (leaves) with Twinspan class IDs

---
+++ R/twinid.R
@@ -0,0 +1,33 @@
+### Twinspan ID for clustering object
+`twinid` <-
+    function(x, ...)
+{
+    merge <- x$merge
+    ## id, leftid, classid are "global" variables which will get
+    ## superassigned (<<-) values in visit()
+    id <- 1
+    leftid <- numeric(nrow(merge))
+    classid <- numeric(nrow(merge) + 1)
+    ## Recursive function to label units (leaves) as Twinspan class IDs
+    visit <- function(i, j) {
+        ## left entry: deeper level, double id and save on left identifiers
+        if (j==1) {
+            id <<- 2*id
+            leftid[i] <<- id
+        } else {
+            ## right entry: same level as left, but increase id by one
+            id <<- leftid[i] + 1
+        }
+        ## leaf: save its id and exit this instance of visit()
+        if (merge[i,j] < 0) {
+            classid[-merge[i,j]] <<- id
+        } else {
+            ## internal node: visit recursively left and right branches
+            visit(merge[i, j], 1)
+            visit(merge[i, j], 2)
+        }
+    }
+    visit(nrow(merge), 1)
+    visit(nrow(merge), 2)
+    classid
+}
