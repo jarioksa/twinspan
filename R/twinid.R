220c006bda6dca81eb362dec64122e1b1e723332
Jari Oksanen <jhoksane@gmail.com>
2021-09-08 14:57:58 +0300

    internal documentation, change twinind arg to hclust

--- R/twinid.R
+++ R/twinid.R
@@ -1,10 +1,21 @@
 ### Twinspan ID for clustering object
 `twinid` <-
-    function(x, ...)
+    function(hclust, ...)
 {
-    merge <- x$merge
+    ## Works with hclust clusters. These have a matrix of dims (n-1)
+    ## times 2, where each row defines a fusion of two items. Negative
+    ## entries -1...-n denote original observations (leaves, terminal
+    ## nodes) and positive entries previous rows of the merge matrix
+    ## (internal nodes). Twinspan in contrast defines topology by an
+    ## integer vector, where the daughters of a mother cluster x are
+    ## 2*x and 2*x+1, and the mother cluster of id z can be found by
+    ## integer division z %/% 2 which allows recontruction of the tree
+    ## topology from a single integer vector.
+    if (!inherits(hclust, "hclust"))
+        stop("'hclust' must be a clustering object from hclust() function")
+    merge <- hclust$merge
     ## id, leftid, classid are "global" variables which will get
-    ## superassigned (<<-) values in visit()
+    ## superassigned (<<-) values in visit().
     id <- 1
     leftid <- numeric(nrow(merge))
     classid <- numeric(nrow(merge) + 1)
@@ -29,7 +40,7 @@
     }
     visit(nrow(merge), 1)
     visit(nrow(merge), 2)
-    ## warn on large IDs: depth > 30
+    ## warn on large IDs: depth > 30, floor(log2(classid)) gives the depths.
     if (max(classid) > .Machine$integer.max)
         warning("some class identifiers larger than integer maximum")
     class(classid) <- "twinid"
