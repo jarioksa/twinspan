82f9e17d39b93098185672caa29885820b6a129f
Jari Oksanen <jhoksane@gmail.com>
2026-08-17 12:50:56 +0300

    twinheat: document (w/ x-refs) and tweak

--- R/twinheat.R
+++ R/twinheat.R
@@ -6,8 +6,25 @@
 #' with quadrat and species dendrograms using \pkg{vegan} function
 #' \code{\link[vegan]{tabasco}}.
 #'
+#' \code{twinheat} is an interface to \pkg{vegan} function
+#' \code{tabasco}. It finds the plotted input data and their
+#' classification in the \code{\link{twinspan}} result object. The
+#' function is modelled after \code{\link{twintable}} and uses the
+#' same arguments. In appearance it is more similar to
+#' \code{\link{image.twinspan}}, but displays individual quadrats and
+#' species instead of terminal groups.
+#'
 #' @seealso \code{\link{twintable}}, \code{\link{image.twinspan}},
 #'     \code{\link[vegan]{tabasco}}.
+#'
+#' @return Returns indices of displayed species and quadrats within
+#'     the subset.
+#'
+#' @examples
+#' data(ahti)
+#' tw <- twinspan(ahti)
+#' twinheat(tw, goodspecies = "both", height = "chi")
+#' twinheat(tw, subset = twingroup(tw, 4), maxspp = 45)
 
 #' @inheritParams twintable
 #' @param height Use either division levels (\code{"level"}), total
@@ -39,7 +56,7 @@
     if (!missing(goodspecies)) {
         if (!missing(maxspp))
             stop("maxspp and goodspecies cannot be defined together")
-        k <- twinspan:::goodspec(object, goodspecies)
+        k <- goodspec(object, goodspecies)
         skept[k] <- TRUE
         skept <- skept & present
     }
@@ -53,5 +70,7 @@
     sptree <- as.dendrogram(object, what = "species", subset = skept,
                             height = height)
     qtree <- as.dendrogram(object, subset = subset, height = height)
-    tabasco(x, use = qtree, sp.ind = sptree, Rowv = Rowv, Colv = Colv, ...)
+    ## gives a large number of warnings about non-binary dendrograms
+    suppressWarnings(
+        tabasco(x, use = qtree, sp.ind = sptree, Rowv = Rowv, Colv = Colv, ...))
 }
