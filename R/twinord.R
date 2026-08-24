e6379fe4590da5fdde5f7709a9e153f21c084848
Jari Oksanen <jhoksane@gmail.com>
2026-08-24 17:15:54 +0300

    implement (a kludgy version) of plot.twinord

--- R/twinord.R
+++ R/twinord.R
@@ -66,10 +66,76 @@
     ord
 }
 
+### non-document print adds two lines to print.decorana
 #' @export
 `print.twinord` <- function(x, ...)
 {
     cat("\nCorrespondence Analysis of twinspan division", x$division, "\n")
     cat("using vegan::decorana with ira=1 and twinspan downweighting\n")
     NextMethod("print")
+}
+
+### plot
+
+#' @rdname twinord
+#'
+#' @param x \code{twinord} result object.
+#' @param quadrats Plot quadrats as text, points or with their
+#'     indicator score or none.
+#' @param pseudospecies Plot text for none, all, indicator
+#'     pseudospecies or valuable species exceeding \code{value}.
+#' @param value Limit of absolute indicator value for displayed
+#'     pseudospecies.
+#' @param qcol Colour of quadrat text in negative and positive groups.
+#' @param \dots Other arguments passed to graphical functions.
+#'
+#' @importFrom graphics points text
+#' @importFrom vegan ordihull
+#'
+#' @export
+`plot.twinord` <- function(x, quadrats = c("text", "points", "score", "none"),
+                           pseudospecies =
+                               c("none", "all", "indicators", "valuable"),
+                           value = 0.25,
+                           qcol = c("blue", "red"), ...)
+{
+    quadrats <- match.arg(quadrats)
+    pseudospecies <- match.arg(pseudospecies)
+    cl12 <- x$class - 2 * x$division + 1
+    ## Handle species data. For appropriate scaling it is best adjust
+    ## data in input x
+    if (pseudospecies == "indicators")
+        x$cproj <- x$cproj[x$indicators, , drop=FALSE]
+    else if (pseudospecies == "valuable") {
+        i <- rev(order(abs(x$indvalues)))
+        indval <- x$indvalues[i]
+        x$cproj <- x$cproj[i,]
+        val <- abs(indval) >= value
+        ## only the best pseudospecies of each species
+        nodup <- !duplicated(gsub("[1-9]$", "", rownames(x$cproj)))
+        x$cproj <- x$cproj[val & nodup, , drop=FALSE]
+    }
+
+    display <- if (pseudospecies == "none" && quadrats == "none")
+                   "none"
+               else if (pseudospecies == "none")
+                   "sites"
+               else if (quadrats == "none")
+                   "species"
+               else
+                   "both"
+
+    pl <- vegan:::plot.decorana(x, display = display, type = "n", ...)
+    switch(quadrats,
+           "text" = text(pl, what = "sites", col = qcol[cl12],
+                         optimize = TRUE, xpd = TRUE, ...),
+           "points" = points(pl, what = "sites", col = qcol[cl12], ...),
+           "score" = text(pl, what = "sites", labels = x$indscores,
+                          col = qcol[cl12], ...),
+           "none" = NULL
+           )
+    if (quadrats != "none")
+        ordihull(x, x$class, draw = "polygon", label = TRUE, cex=1)
+    if (pseudospecies != "none")
+        text(pl, what = "species", optimize=TRUE, xpd = TRUE)
 }
